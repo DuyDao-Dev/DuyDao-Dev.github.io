5fb8aaa917826214fe9d67f4ca88d4adb21496eb
Duy Dao <jimmydao@gmail.com>
2021-08-22 16:08:21 -0500

    first commit

---
+++ src/data.js
@@ -0,0 +1,52 @@
+export const projects = [
+  {
+    title: "SQL ToDo List",
+    subtitle: "SQL ToDo List",
+    description:
+      "Early app I created to keep track of things I needed to do.",
+    image: "./project-4.gif",
+    link: "https://https://github.com/DuyDao-Dev/weekend-sql-to-do-list.com",
+  },
+  {
+    title: "Guess Who",
+    subtitle: "jQuery",
+    description:
+      "Fun Guessing Game",
+    image: "./guess.gif",
+    link: "https://github.com/DuyDao-Dev/guess-who",
+  },
+  {
+    title: "No Thyme",
+    subtitle: "React, Redux, Saga",
+    description:
+      "No Thyme: Search for recipes with ingredients you have on hand. Where No Thyme, saves time.",
+    image: "./thyme.jpeg",
+    link: "https://github.com/DuyDao-Dev/prime-solo-project-2021",
+  },
+];
+
+// export const testimonials = [
+//   {
+//     quote:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+//     image: "https://randomuser.me/api/portraits/men/1.jpg",
+//     name: "Jesse Hicks",
+//     company: "Zoozle",
+//   },
+//   {
+//     quote:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+//     image: "https://randomuser.me/api/portraits/men/94.jpg",
+//     name: "Ruben Alvarez",
+//     company: "Dooble.io",
+//   },
+// ];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL",
+  "Material UI",
+];
